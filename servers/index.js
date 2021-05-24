426ef509edc3e3d934f47508ff701d1abe7c3cdb
Candice Canoso <mail@cndce.me>
2021-05-25 00:36:17 +0800

    Cleanup for deployment

--- servers/index.js
+++ servers/index.js
@@ -1,6 +1,3 @@
-// import ClientServer from "./ClientServer.mjs";
-// import WebSocketServer from "./WebSocketServer.mjs";
-
 const ClientServer = require("./ClientServer");
 const WebSocketServer = require("./WebSocketServer");
 
