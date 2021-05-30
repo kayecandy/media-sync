770af191995d1c5a2c622dfa761385fd616b5456
Candice Canoso <mail@cndce.me>
2021-05-30 20:06:55 +0800

    [MISC] Added TEST_MODE env; Added error.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var indexRouter = express.Router();
 indexRouter.get("/", function (req, res, next) {
   res.render("index", {
     wsUrl: process.env.URL_WS + ":" + process.env.PORT_WS,
+    jsTestMode: process.env.JS_TEST_MODE,
   });
   next();
 });
