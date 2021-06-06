a98e1b5b7cdf8a9998f3a5ca2c59b1036df2428a
Candice Canoso <mail@cndce.me>
2021-06-06 17:14:35 +0800

    Updated deploy scripts to add .env.deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 eval $(egrep -v '^#' .env | xargs)
 
-ssh -t -i "$SSH_KEY_FILE" $SSH_USERNAME@$SSH_SERVER -p $SSH_PORT "cd ~/media-sync; git pull; pm2 delete media-sync; pm2 --name media-sync start npm -- start; pm2 logs"
+scp -i "$SSH_KEY_FILE" -P $SSH_PORT .env.deploy  $SSH_USERNAME@$SSH_SERVER:~/media-sync
+
+ssh -t -i "$SSH_KEY_FILE" $SSH_USERNAME@$SSH_SERVER -p $SSH_PORT "cd ~/media-sync; git pull; pm2 delete media-sync; mv .env.deploy .env; npm install; pm2 --name media-sync start npm -- start; pm2 logs"
