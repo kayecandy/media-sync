7522ed354abe76fbe6c80ef959bcf3256fafd715
Candice Canoso <mail@cndce.me>
2021-06-06 23:23:06 +0800

    [BUG FIX] Corrected git command in deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ eval $(egrep -v '^#' .env | xargs)
 
 scp -i "$SSH_KEY_FILE" -P $SSH_PORT .env.deploy  $SSH_USERNAME@$SSH_SERVER:~/media-sync
 
-ssh -t -i "$SSH_KEY_FILE" $SSH_USERNAME@$SSH_SERVER -p $SSH_PORT "cd ~/media-sync; git pull; pm2 delete media-sync; mv .env.deploy .env; npm install; pm2 --name media-sync start npm -- start; pm2 logs"
+ssh -t -i "$SSH_KEY_FILE" $SSH_USERNAME@$SSH_SERVER -p $SSH_PORT "cd ~/media-sync; git fetch; git checkout origin/master; pm2 delete media-sync; mv .env.deploy .env; npm install; pm2 --name media-sync start npm -- start; pm2 logs"
